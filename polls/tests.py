09c1f96678f1daba2a029863e928bbec163fe84a
Thecapstone <thecapstonemogbolu@gmail.com>
2025-11-28 11:18:05 +0100

    included selenium live testing, and a css file for basic styling

--- polls/tests.py
+++ polls/tests.py
@@ -37,14 +37,14 @@ class QuestionModelTests(TestCase):
     
 
 
-def create_question(question_text, choice_text, days):
+def create_question(question_text, days):
     """
     Create a question with the given `question_text` and published the
     given number of `days` offset to now (negative for questions published
     in the past, positive for questions that have yet to be published).
     """
     time = timezone.now() + datetime.timedelta(days=days)
-    return Question.objects.create(question_text=question_text, choice_text=choice_text, pub_date=time)
+    return Question.objects.create(question_text=question_text, pub_date=time)
 
 
 class QuestionIndexViewTests(TestCase):
@@ -112,12 +112,13 @@ class QuestionDetailViewTests(TestCase):
         """
         The detail view of a question that has no choices should return a 404.
         """
-        question = Question.objects.create(question_text="No choices")
-
-        url = reverse("polls:detail", args=[question.id])
+        no_choice_question = create_question(question_text="No choices", days=-5)
+        
+        url = reverse("polls:detail", args=(no_choice_question.id,))
         response = self.client.get(url)
         
-        self.assertQuerySetEqual(response.status_code, 404)
+        self.assertEqual(response.status_code, 404)
+
 
     def test_future_question(self):
         """
@@ -135,8 +136,7 @@ class QuestionDetailViewTests(TestCase):
         displays the question's text.
         """
         past_question = create_question(question_text="Past Question.", days=-5)
+        Choice.objects.create(question=past_question, choice_text="Yes")
         url = reverse("polls:detail", args=(past_question.id,))
         response = self.client.get(url)
         self.assertContains(response, past_question.question_text)
-
-    
